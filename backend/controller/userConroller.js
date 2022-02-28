7391f35df36ae6c074f731bdbe9f69423d7f05f5
khalil-codes <khalilpati55@gmail.com>
2022-02-28 12:28:36 +0530

    Added user model, controller and routes

---
+++ backend/controller/userConroller.js
@@ -0,0 +1,96 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Name, email or passoword field(s) missing");
+    }
+    // Check is user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User with same email exists. Please Login");
+    }
+    // Hash The Passoword
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+    const user = await User.create({ name, email, password: hashedPassword });
+    if (user) {
+        res.status(201).json({
+            status: "success",
+            data: {
+                user: {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    token: generateToken(user._id),
+                },
+            },
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
+
+// @desc    Authenticate user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Email or passoword field(s) missing");
+    }
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        res.status(400);
+        throw new Error("User with email doesn't exist. Please Register");
+    }
+    // Check Password
+    if (await bcrypt.compare(password, user.password)) {
+        res.status(201).json({
+            status: "success",
+            data: {
+                user: {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    token: generateToken(user._id),
+                },
+            },
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid Password Try Again");
+    }
+});
+
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            message: "User data",
+        },
+    });
+});
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+module.exports = { registerUser, loginUser, getMe };
