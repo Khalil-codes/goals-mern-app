add3662120148605cb6970ec1821e633a068dac2
khalil-codes <khalilpati55@gmail.com>
2022-03-01 13:20:55 +0530

    Made some UI tweaks

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout, reset } from "../redux/auth/authSlice";
+import { useAuth } from "../redux/store";
 
 const Header = () => {
+    const { user } = useAuth();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate("/");
+    };
     return (
         <header className="header">
             <div className="logo">
                 <Link to="/">Goalsy</Link>
             </div>
             <ul>
-                <li>
-                    <Link to="/login">
-                        <FaSignInAlt /> Login
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/register">
-                        <FaUser /> Register
-                    </Link>
-                </li>
-                {/* <li>
-                    <FaSignOutAlt /> Logout
-                </li> */}
+                {!user ? (
+                    <>
+                        <li>
+                            <Link to="/login">
+                                <FaSignInAlt /> Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/register">
+                                <FaUser /> Register
+                            </Link>
+                        </li>
+                    </>
+                ) : (
+                    <li>
+                        <button className="btn" onClick={handleLogout}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                )}
             </ul>
         </header>
     );
